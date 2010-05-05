8d8c9a7c95abea33e613ab06bc9dc3ca75adf7fb
Jeremy Mates <jmates@sial.org>
2010-05-05 06:06:00 +0000

    Move entry processing to private methods. Additional work on option processing methods.

--- lib/Config/OpenSSH/Authkey/Entry.pm
+++ lib/Config/OpenSSH/Authkey/Entry.pm
@@ -1,34 +1,44 @@
 # -*- Perl -*-
 #
-# Representation of individual OpenSSH authorized_keys entries. Based on
+# Representation of individual OpenSSH authorized_keys entries, based on
 # a study of the sshd(8) manual, along with the OpenSSH 5.2 sources.
+# This module only weakly validates the data presented; in particular,
+# no effort is made to confirm whether the key options are actual valid
+# options for the version of OpenSSH in question.
 
 package Config::OpenSSH::Authkey::Entry;
 
-use warnings;
 use strict;
+use warnings;
 
 use Carp qw(croak);
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # This limit is set for various things under OpenSSH code. Used here to
 # limit length of authorized_keys lines.
 my $SSH_MAX_PUBKEY_BYTES = 8192;
 
-sub new {
-  my $class = shift;
-  my $self  = {};
+######################################################################
+#
+# Data Parsing Methods - Internal
+
+# TODO should this create a Config::OpenSSH::Authkey::Entry::Options object?
+my $_split_options = sub { my $self = shift; my $options = shift; };
+
+my $_parse_entry = sub {
+  my $self  = shift;
   my $entry = shift || q{};
+  my $prefs = shift || {};
 
   my ( $options, $key, $comment, $protocol );
 
   chomp $entry;
 
   if ( $entry =~ m/^\s*$/ or $entry =~ m/^\s*#/ ) {
-    croak('no public key data');
+    return ( 0, 'no public key data' );
   } elsif ( length $entry >= $SSH_MAX_PUBKEY_BYTES ) {
-    croak('exceeds size limit');
+    return ( 0, 'exceeds size limit' );
   }
 
   # OpenSSH supports leading whitespace before options or key. Strip
@@ -74,18 +84,50 @@ UBLE: {
   }
 
   if ( !defined $key ) {
-    croak('unable to parse public key');
+    return ( 0, 'unable to parse public key' );
 
   } else {
+    $self->{_key}      = $key;
+    $self->{_protocol} = $protocol;
+
     if ( defined $options ) {
       $options =~ s/\s*$//;
       $self->{_options} = $options;
+
+      if (  exists $prefs->{parse_options}
+        and $prefs->{parse_options}
+        and length $options > 0 ) {
+        my ( $is_parsed, $err_msg ) = $_split_options->( $self, $options );
+        if ( !$is_parsed ) {
+          return ( 0, $err_msg );
+        }
+      }
     }
-    $self->{_key}      = $key;
-    $self->{_protocol} = $protocol;
+
     if ( defined $comment ) {
       $comment =~ s/\s*$//;
       $self->{_comment} = $comment;
+    }
+  }
+
+  return ( 1, 'ok' );
+};
+
+######################################################################
+#
+# Class methods
+
+sub new {
+  my $class = shift;
+  my $entry = shift;
+  my $prefs = shift || {};
+
+  my $self = {};
+
+  if ( defined $entry ) {
+    my ( $is_parsed, $err_msg ) = $_parse_entry->( $self, $entry, $prefs );
+    if ( !$is_parsed ) {
+      croak($err_msg);
     }
   }
 
@@ -96,6 +138,25 @@ UBLE: {
 ######################################################################
 #
 # Instance methods
+
+# Utility method for folks who want to do a ->new and then at some later
+# point throw an entry onto the object.
+sub parse {
+  my $self  = shift;
+  my $entry = shift;
+  my $prefs = shift || {};
+
+  if ( exists $self->{_key} ) {
+    croak('object has already parsed an entry');
+  }
+
+  my ( $is_parsed, $err_msg ) = $_parse_entry->( $self, $entry, $prefs );
+  if ( !$is_parsed ) {
+    croak($err_msg);
+  }
+
+  return $self;
+}
 
 sub key {
   shift->{_key};
@@ -114,27 +175,61 @@ sub comment {
   return $self->{_comment};
 }
 
-# TODO also support submodule that provides a programmatic interface to
-# these? E.g. if ->can, pull as_string from it in turn.
 sub options {
   my $self    = shift;
   my $options = shift;
-  if ( defined $options ) {
+  my $set_options;
+
+  if ( exists $self->{_parsed_options} ) {
+    die('TODO implement get/set of options after options parsed');
+
+  } elsif ( defined $options ) {
     $self->{_options} = $options;
+    $set_options = $options;
   }
-  return $self->{_options};
+
+  return $set_options;
 }
 
-# TODO may need option for comparison of this object with some other
-# object (or authkey entry, if passed a string)?? (This would compare
-# the key if only the key, or perhaps also the options, though easier to
-# only use the key for starters.)
+sub get_option {
+  my $self = shift;
+  my $value;
+
+  if ( exists $self->{_options} and length $self->{_options} > 0 ) {
+    if ( !exists $self->{_parsed_options} ) {
+      my ( $is_parsed, $err_msg ) =
+        $_split_options->( $self, $self->{_options} );
+      if ( !$is_parsed ) {
+        croak($err_msg);
+      }
+    }
+    # TODO - now have parsed options, need to lookup the option from the
+    # whatever, return value, if any..
+  }
+
+  return $value;
+}
+
+# TODO how distinguish between boolean and value options? Or don't,
+# and leave it up to the caller to pass no data in for a boolean
+# option? Hrm.
+sub set_option {
+  my $self         = shift;
+  my $option_name  = shift;
+  my $option_value = shift;
+
+  # parse option string, if any, to internal format. update value or
+  # create new option, depending.
+}
 
 sub as_string {
   my $self   = shift;
   my $string = q{};
 
-  if ( exists $self->{_options} and length $self->{_options} > 0 ) {
+  if ( exists $self->{_parsed_options} ) {
+    # TODO should this be a as_string on a ::Options object?
+    $string .= 'TODO' . q{ };
+  } elsif ( exists $self->{_options} and length $self->{_options} > 0 ) {
     $string .= $self->{_options} . q{ };
   }
   $string .= $self->{_key};
@@ -148,9 +243,6 @@ sub as_string {
 1;
 
 __DATA__
-
-TODO for the option parsing code... this should probably live in a
-different package.
 
 # Delved from sshd(8), auth-options.c of OpenSSH 5.2. Insensitive match
 # required, as OpenSSH uses strncasecmp(3).
@@ -233,7 +325,7 @@ __END__
 
 =head1 NAME
 
-Config::OpenSSH::Authkey::Entry - authorized_keys file entry
+Config::OpenSSH::Authkey::Entry - authorized_keys entry handler
 
 =head1 SYNOPSIS
 
@@ -241,7 +333,7 @@ Config::OpenSSH::Authkey::Entry - authorized_keys file entry
 
 =head1 DESCRIPTION
 
-This module parses input OpenSSH authorized_keys lines.
+This module parses input OpenSSH authorized_keys lines. TODO
 
 =head1 METHODS
 
@@ -272,7 +364,7 @@ Jeremy Mates, E<lt>jmates@sial.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2009 by Jeremy Mates.
+Copyright 2009-2010 by Jeremy Mates.
 
 This program is free software; you can redistribute it and/or modify it
 under the Artistic license.
